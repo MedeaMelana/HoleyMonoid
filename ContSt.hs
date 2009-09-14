5ba9e3c98dfab6a0c4303184c69096a0225e6b9d
MedeaMelana <MedeaMelana@da10e38e-a12b-11de-86bd-4589e38204d3>
2009-09-14 15:12:57 +0000

    Renamed some type variables.

--- ContSt.hs
+++ ContSt.hs
@@ -35,12 +35,12 @@ instance (Functor f, Functor g) => Functor (f :.: g) where
 
 -- Unwrapping of functors.
 
-data Apply f a b where
-  ApplyId   :: Apply Id b b
-  ApplyArr  :: Apply (Arr a) b (a -> b)
+data Apply f r a where
+  ApplyId   :: Apply Id r r
+  ApplyArr  :: Apply (Arr a) r (a -> r)
   ApplyComp :: Functor f => Apply f b c -> Apply g a b -> Apply (f :.: g) a c
 
-apply :: Apply f a b -> f a -> b
+apply :: Apply f r a -> f r -> a
 apply ApplyId         = runId
 apply ApplyArr        = runArr
 apply (ApplyComp f g) = apply f . fmap (apply g) . runComp
@@ -50,28 +50,28 @@ apply (ApplyComp f g) = apply f . fmap (apply g) . runComp
 -- The Format category.
 
 -- | A continuation-style monoid wrapper. Composition is possible through the ('.') operator in type class 'Category'.
-data ContSt m a b where
-  ContSt :: Functor f => Apply f a b -> f m -> ContSt m a b
+data ContSt m r a where
+  ContSt :: Functor f => Apply f r a -> f m -> ContSt m r a
 
 instance Monoid m => Category (ContSt m) where
-  id = ContSt ApplyId (Id mempty)
+  id                        = ContSt ApplyId (Id mempty)
   ContSt af f . ContSt ag g = ContSt (ApplyComp af ag) (f <> g)
 
 (<>) :: (Functor f, Functor g, Monoid m) => f m -> g m -> (f :.: g) m
 f <> g = Comp (fmap (\s -> fmap (mappend s) g) f)
 
 -- | Wrap a constant.
-now :: m -> ContSt m b b
+now :: m -> ContSt m r r
 now = ContSt ApplyId . Id
 
 -- | Expect an argument.
-later :: (a -> m) -> ContSt m b (a -> b)
+later :: (a -> m) -> ContSt m r (a -> r)
 later = ContSt ApplyArr . Arr
 
 -- | Map a continuation to a different monoid.
-mapContSt :: (m -> n) -> ContSt m a b -> ContSt n a b
+mapContSt :: (m -> n) -> ContSt m r a -> ContSt n r a
 mapContSt g (ContSt af f) = ContSt af (fmap g f)
 
 -- | Run the continutation, producing the resulting monoid.
-runContSt :: ContSt m m b -> b
+runContSt :: ContSt m m a -> a
 runContSt (ContSt af f) = apply af f
