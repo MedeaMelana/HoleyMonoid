6bf9c44a89458cacccd093fba362b5e1287b9224
MedeaMelana <MedeaMelana@da10e38e-a12b-11de-86bd-4589e38204d3>
2009-09-16 09:40:31 +0000

    Greatly simplified thanks to some pointers in replies from David Menendez and Derek Elkins. http://thread.gmane.org/gmane.comp.lang.haskell.cafe/63409/focus=63489 and next.

--- ContSt.hs
+++ ContSt.hs
@@ -17,51 +17,25 @@ import Data.Monoid
 
 
 
--- Functor building blocks.
-
-newtype Id a = Id { runId :: a }
-instance Functor Id where
-  fmap f (Id x) = Id (f x)
-
-newtype Arr a b = Arr { runArr :: a -> b }
-instance Functor (Arr a) where
-  fmap f (Arr g) = Arr (f . g)
-
-newtype (:.:) f g a = Comp { runComp :: f (g a) }
-instance (Functor f, Functor g) => Functor (f :.: g) where
-  fmap f (Comp fga) = Comp (fmap (fmap f) fga)
-
-
-
 -- The Format category.
 
--- | A continuation-style monoid wrapper. Composition is possible through the ('.') operator in type class 'Category'.
-data ContSt m r a where
-  ContSt :: Functor f => (f r -> a) -> f m -> ContSt m r a
+newtype ContSt m r a = Cont { runCont :: (m -> r) -> a }
+
+now :: m -> ContSt m r r
+now a = Cont $ \k -> k a
+
+bind :: ContSt m b c -> (m -> ContSt n a b) -> ContSt n a c
+m `bind` f = Cont $ \k -> runCont m (\a -> runCont (f a) k)
+
+later :: (a -> m) -> ContSt m r (a -> r)
+later f = Cont $ \k -> k . f
 
 instance Monoid m => Category (ContSt m) where
   id = now mempty
-  ContSt apf f . ContSt apg g =
-    ContSt (apf . fmap apg . runComp) (f <> g)
+  f . g = f `bind` \a -> g `bind` \b -> now (a `mappend` b)
 
-instance Functor (ContSt m r) where
-  fmap g (ContSt apf f) = ContSt (g . apf) f
-
-(<>) :: (Functor f, Functor g, Monoid m) => f m -> g m -> (f :.: g) m
-f <> g = Comp (fmap (\s -> fmap (mappend s) g) f)
-
--- | Wrap a constant.
-now :: m -> ContSt m r r
-now = ContSt runId . Id
-
--- | Expect an argument.
-later :: (a -> m) -> ContSt m r (a -> r)
-later = ContSt runArr . Arr
-
--- | Map a continuation to a different monoid.
 mapContSt :: (m -> n) -> ContSt m r a -> ContSt n r a
-mapContSt g (ContSt ap f) = ContSt ap (fmap g f)
+mapContSt g m = Cont (\k -> runCont m (k . g))
 
--- | Run the continuation, producing the resulting monoid.
 runContSt :: ContSt m m a -> a
-runContSt (ContSt apf f) = apf f
+runContSt m = runCont m id
